22488026ceb06edfeabef950203867b66cb44ee8
Haris131 <41672454+Haris131@users.noreply.github.com>
2022-08-28 15:47:51 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,6 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Intel XMM 7650 LTE-A Pro modem.
+svn co https://github.com/Haris131/xmm-modem/trunk package/xmm-modem
