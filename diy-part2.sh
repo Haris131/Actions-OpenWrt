aa44ffca8bfbca1b2e4fa7fa9078f9d7462bc481
Haris131 <41672454+Haris131@users.noreply.github.com>
2023-01-20 18:18:42 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,18 +13,43 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Intel XMM 7650 LTE-A Pro modem.
-#svn co https://github.com/Haris131/xmm-modem/trunk package/xmm-modem
-
 # Modemfeed
 svn co https://github.com/koshev-msk/modemfeed/trunk/luci package/luci
-svn co https://github.com/Haris131/modemfeed/trunk/packages package/packages
+svn co https://github.com/koshev-msk/modemfeed/trunk/packages package/packages
 
-# Clone community packages to package/community
-mkdir package/community
-pushd package/community
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
-# Add OpenClash
-git clone --depth=1 -b dev https://github.com/vernesong/OpenClash
+# Add luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 
-popd
+# Add luci-app-diskman
+svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-diskman package/luci-app-diskman
+
+# Add luci-app-adguardhome
+git clone --depth=1 https://github.com/yang229/luci-app-adguardhome package/luci-app-adguardhome
+
+# Add luci-app-temp-status
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+
+# Add luci-app-temp-status
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status package/luci-app-temp-status
+
+#-----------------------------------------------------------------------------
+#   Start of @helmiau terminal scripts additionals menu
+#-----------------------------------------------------------------------------
+HWOSDIR="package/base-files/files"
+rawgit="https://raw.githubusercontent.com"
+[ ! -d $HWOSDIR/usr/bin ] && mkdir -p $HWOSDIR/usr/bin
+
+# Add ram checker from wegare123
+# run "ram" using terminal to check ram usage
+wget --no-check-certificate -qO $HWOSDIR/bin/ram "$rawgit/wegare123/ram/main/ram.sh"
+
+# Add ocmetawss: "OpenClash Websocket Mod Core" script downloader
+# run "ocmetawss" using terminal to check ram usage
+wget --no-check-certificate -qO $HWOSDIR/bin/ocmetawss "$rawgit/helmiau/openwrt-config/main/others/ocmetawss"
+
+# Add fix download file.php for xderm and libernet
+# run "fixphp" using terminal for use
+wget --no-check-certificate -qO $HWOSDIR/bin/fixphp "$rawgit/helmiau/openwrt-config/main/fix-xderm-libernet-gui"
