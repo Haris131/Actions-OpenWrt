211a3a7db8bbace0eb79ff6b5ed922195c7751ee
Haris131 <41672454+Haris131@users.noreply.github.com>
2023-03-30 18:47:26 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -42,8 +42,8 @@ git clone --depth=1 https://github.com/friyadhibiermann/shc_openwrt.git package/
 git clone --depth=1  https://github.com/kuoruan/openwrt-upx.git package/openwrt-upx
 
 # HelmiWrt packages
-git clone --depth=1 https://github.com/Haris131/helmiwrt-packages package/hemliwrt-packages
-rm -rf helmiwrt-packages/luci-app-v2raya
+git clone --depth=1 https://github.com/Haris131/helmiwrt-packages package/helmiwrt-packages
+rm -rf feeds/packages/net/redsocks
 
 #-----------------------------------------------------------------------------
 #   Start of @helmiau terminal scripts additionals menu
